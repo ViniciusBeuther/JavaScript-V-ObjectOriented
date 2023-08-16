d451a1f7cf7d9f6c96adfd3d618ffe7ab0592efd
ViniciusBeuther <vinicius.beuther15@gmail.com>
2023-08-16 12:58:32 -0300

    Encapsulation
    Trying to make an example works, just for fun and studies.

--- Encapsulamento/index.js
+++ Encapsulamento/index.js
@@ -1,22 +1,43 @@
 class user {
     #password
     #balance = 0
+    #email
     constructor(email, password){
-        this.email = email
+        this.#email = email
         this.#password = password
     }
 
-    getPassword(email, password){
-        if (email === this.email && password === this.password){
-            return this.#password
-        }
+    login(email, password){
+        const currentEmail = this.#email
+        const currentPassword = this.#password
 
-        else{
-            return null
-        }
+            if( currentEmail == email && currentPassword == password ){
+                const sucess = `\t\t===== SUCESSO =====\n
+                Email: ${currentEmail}\n
+                Password: ${password}\n
+                ===================`
+                
+                return sucess
+            }
+            else{
+                return 'Login inválido!'
+            }
+    }
+
+    getPassword(){
+        return this.#password
+    }
+
+    getEmail(){
+        return this.#email
     }
 }
 
-vinicius = new user('vinicius.beuther15@gmail.com', 'vinicius123')
+vinicius = new user('viniciusTest@gmail.com', 'testing123')
+carla = new user('carlaTest@gmail.com', 'testing123')
 
-console.log(vinicius.getPassword('vinicius.beuther15@gmail.com', 'vinicius123'))
+//console.log(vinicius.getEmail())
+//console.log(vinicius.getPassword())
+console.log(vinicius.login('viniciusTesting@gmail.com', 'test123'))
+console.log(carla.login('carlaTest@gmail.com', 'testing123'))
+
---
+++ Herança/index.js
@@ -0,0 +1,31 @@
+class Property {
+    constructor(area, price){
+        this.area = area
+        this.price = price
+    }
+
+    getPricePerSquareMeter() {
+        return this.price / this.area
+    }
+}
+
+class Apartment extends Property {
+    constructor (number, area, price){
+        super(area, price) // Call the constructor to set initial properties
+        this.number = number
+    }
+
+    getFloor(){
+        return this.number.slice(0, -2) //slice is to remove the last 2 digits of the string
+    }
+}
+
+class House extends Property { } // Herança
+const land = new Property(100, 100000) // Creating a land
+const someHouse = new House(120, 200000) //Creating a house object, son of Property class
+const myApartment = new Apartment("201", 100, 160000) // Creating a new apartment, son of Property class
+
+console.log(land.getPricePerSquareMeter())
+console.log(someHouse.getPricePerSquareMeter())
+console.log(myApartment.getFloor())
+
