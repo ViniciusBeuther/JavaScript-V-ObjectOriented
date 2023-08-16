8689b10385bf0f628e474e45151c54c4a504e518
ViniciusBeuther <vinicius.beuther15@gmail.com>
2023-08-16 13:05:11 -0300

    Heritage and exercice 20
    Leaning about heritage and how use it in javascript

--- Herança/index.js
+++ Herança/index.js
@@ -23,9 +23,9 @@ class Apartment extends Property {
 class House extends Property { } // Herança
 const land = new Property(100, 100000) // Creating a land
 const someHouse = new House(120, 200000) //Creating a house object, son of Property class
-const myApartment = new Apartment("201", 100, 160000) // Creating a new apartment, son of Property class
+const myApartment = new Apartment("201", 100, 160000) // Creating a new apartment, son of Property class, using Super to load the constructor
 
 console.log(land.getPricePerSquareMeter())
 console.log(someHouse.getPricePerSquareMeter())
-console.log(myApartment.getFloor())
+console.log(myApartment.getFloor()) 
 
